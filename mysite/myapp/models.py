5adc8024cde0b6c71070eacf178494a303492121
Donald Turner <donaldturner65@gmail.com>
2021-03-01 15:18:41 -0800

    Revert "Merge branch 'main' of https://github.com/lukedoukakis/software-engineering-project into main"
    This reverts commit 4e417ce64ce6b919e2b764bbc115902e5f0931dc, reversing
    changes made to 346cedeb99a6754da8caec316b4b4a9cb9171c64.

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -22,24 +22,24 @@ from django.db import models
 
 
 
-# # represents a table
-# class Test(models.Model):
+# represents a table
+class Test(models.Model):
 
-#     # fields/columns for the table
-#     text = models.CharField(max_length=10)
-#     number = models.IntegerField(null=True)
-#     url = models.URLField(default='www.example.com')
+    # fields/columns for the table
+    text = models.CharField(max_length=10)
+    number = models.IntegerField(null=True)
+    url = models.URLField(default='www.example.com')
 
-#     # specifies that the id of an entry will be its number (optional)
-#     def __str__(self):
-#         return self.text
+    # specifies that the id of an entry will be its number (optional)
+    def __str__(self):
+        return self.text
 
 
-# # this one takes an entry from another table as a field
-# class ForeignKeyExample(models.Model):
-#     name = models.CharField(max_length=10)
-#     number = models.IntegerField(null=True)
-#     test = models.ForeignKey(Test, on_delete=models.CASCADE)
+# this one takes an entry from another table as a field
+class ForeignKeyExample(models.Model):
+    name = models.CharField(max_length=10)
+    number = models.IntegerField(null=True)
+    test = models.ForeignKey(Test, on_delete=models.CASCADE)
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
