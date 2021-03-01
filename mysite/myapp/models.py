3edc52fef06a46e174d5778882917943b5077221
Donald Turner <donaldturner65@gmail.com>
2021-03-01 15:22:08 -0800

    Revert "models testing and comments"
    This reverts commit 1590bef05d57d2da67ad64d0506fefefb4ad0f3d.

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+<<<<<<< HEAD
+=======
+
+# represents a table
+class Testtable(models.Model):
+    text = models.CharField(max_length=10)
+>>>>>>> parent of 1590bef... models testing and comments
